0982ede63f0748a87cdfc7d9ec68767c2f9be2ab
alejandroesquivel86 <miguelesquivel86@gmail.com>
2022-06-09 21:02:20 -0300

    Eventos

--- monotributo.js
+++ monotributo.js
@@ -1,89 +1,99 @@
 //CALCULO DE CATEGORIA
-let categoriaCliente
+let categoriaCliente;
+let ingresos = document.getElementById("ingresoAnualCliente");
+let botoncalcular = document.getElementById("btnCalcular");
 
-function calcularCategoria(){
-    let ingresos= parseInt(prompt("Colocá los Ingresos Brutos de los últimos 12 meses. Ej: 900000"));    
-        if(ingresos != 0){   
+botoncalcular.addEventListener("click",calcularCategoria)
+
+function calcularCategoria(){      
+        if(ingresos.value != 0){   
             switch (true) {
-                case (ingresos < 466201):
-                    alert("Categoría A");
+                case (ingresos.value < 466201):
+                    console.log("Categoría A");
                     categoriaCliente = "A";
-                    console.log("Categoría "+ categoriaCliente+ ": "+ingresos);
+                    document.getElementById("categoriaCalculada").value = "A";
                     break;
-                case (466202 < ingresos)&&(ingresos < 693002):                
-                        alert("Categoría B");
+                case (466202 < ingresos.value)&&(ingresos.value < 693002):                
+                        console.log("Categoría B");
                         categoriaCliente = "B";
-                        console.log("Categoría "+ categoriaCliente+ ": "+ingresos);
+                        document.getElementById("categoriaCalculada").value = "B";
                         break;
-                        case (693003 < ingresos)&&(ingresos < 970203): 
-                        alert("Categoría C");
-                        categoriaCliente = "C";
-                        console.log("Categoría "+ categoriaCliente+ ": "+ingresos);
-                        break;
-                default:
-                    alert("Otra Categoría");
-                    categoriaCliente = "'Fuera Monotributo'";
-                    console.log("Categoría "+ categoriaCliente+ ": "+ingresos);
+                case (693003 < ingresos.value)&&(ingresos.value < 970203): 
+                    console.log("Categoría C");
+                    categoriaCliente = "C";
+                    document.getElementById("categoriaCalculada").value = "C";
+                    break;
+                default:                    
+                    categoriaCliente = "'Fuera Monotributo'";                    
                     break;
             }  
         } else {
-            alert("Ingresar un valor");
-            console.log("No ingreso valor");
-        }
-        return (ingresos)
-        
-}
+            alert("Ingresar un valor");            
+        };
+    };
 
 //CARGA DE CLIENTES
 
-class ClienteNuevo {
-    constructor (razonSocial,direccion,cuit,ingresoAnual,categoriaClase){
-        this.razonSocial=razonSocial.toUpperCase();
-        this.direccion=direccion.toUpperCase();
-        this.cuit=cuit;
-        this.ingresoAnual=ingresoAnual;
-        this.categoriaClase=categoriaClase;
-    }
-    mostrarCliente=()=>console.log("Razon Social: "+this.razonSocial+
-                                    " direccion:"+this.direccion+
-                                    " CUIT:"+this.cuit+
-                                    " Ingreso Anual:"+this.ingresoAnual+
-                                    " Categoria:"+this.categoriaClase);
-}
+// class ClienteNuevo {
+//     constructor (razonSocial,direccion,cuit,ingresoAnual,categoriaClase){
+//         this.razonSocial=razonSocial.toUpperCase();
+//         this.direccion=direccion.toUpperCase();
+//         this.cuit=cuit;
+//         this.ingresoAnual=ingresoAnual;
+//         this.categoriaClase=categoriaClase;
+//     }
+//     mostrarCliente=()=>console.log("Razon Social: "+this.razonSocial+
+//                                     " direccion:"+this.direccion+
+//                                     " CUIT:"+this.cuit+
+//                                     " Ingreso Anual:"+this.ingresoAnual+
+//                                     " Categoria:"+this.categoriaClase);
+// }
 
-//array
-const clientes = [];
+// //array
+// const clientes = [];
 
-for (let i = 1; i < 2; i++){
-    alert("podrá ingresar "+ (2-i) +" clientes");
-    clientes.push(new ClienteNuevo(prompt("Razón Social"), prompt("Direccion"),prompt("CUIT"),calcularCategoria(),categoriaCliente));    
-}
+// for (let i = 1; i < 2; i++){
+//     alert("podrá ingresar "+ (2-i) +" clientes");
+//     clientes.push(new ClienteNuevo(prompt("Razón Social"), prompt("Direccion"),prompt("CUIT"),calcularCategoria(),categoriaCliente));    
+// }
 
-clientes.forEach((cliente)=>{alert("Se agregó " +cliente.razonSocial+" - CUIT: "+cliente.cuit+" que según ingresos anuales le corresponde la categoría de monotributo: '"+cliente.categoriaClase+"'")});
+// clientes.forEach((cliente)=>{alert("Se agregó " +cliente.razonSocial+" - CUIT: "+cliente.cuit+" que según ingresos anuales le corresponde la categoría de monotributo: '"+cliente.categoriaClase+"'")});
 
 
-for (const cliente of clientes)
-cliente.mostrarCliente();
+// for (const cliente of clientes)
+// cliente.mostrarCliente();
 
 //calcularCategoria();
 
-//ETELEMENTSBYCLASSNAME
+//getelementby...
 let grilla=document.getElementById("grillaMonotributo");
 let titulo=document.getElementById("titulo");
 let categorias = document.getElementsByClassName("categoria");
-let ingresos = document.getElementsByClassName("ingresoCategoria");
+let ingresosCategoria = document.getElementsByClassName("ingresoCategoria");
 let boton=document.getElementById("boton");
 
 //para pintar la grilla
 function pintarGrilla(){
+    if (boton.innerText=="Colorear Grilla") {
     grilla.style.background="black";
     titulo.style.color="red";
     for(const categoria of categorias){
         categoria.style.color="white";    
     }
-    for(const ingreso of ingresos){
+    for(const ingreso of ingresosCategoria){
         ingreso.style.color="white";    
     }
-    boton.innerText="Despintar"
+    boton.innerText="Despintar"   
+} else {
+    grilla.style.background="white";
+    titulo.style.color="black";
+    for(const categoria of categorias){
+        categoria.style.color="black";    
+    }
+    for(const ingreso of ingresosCategoria){
+        ingreso.style.color="black";    
+    }
+    boton.innerText="Colorear Grilla"  
+} 
 }
 
