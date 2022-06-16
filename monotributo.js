e6513d889a46f9cd27f3562f6170930eab58a390
alejandroesquivel86 <miguelesquivel86@gmail.com>
2022-06-16 18:41:16 -0300

    2da Entrega

--- monotributo.js
+++ monotributo.js
@@ -97,3 +97,56 @@ function pintarGrilla(){
 } 
 }
 
+//Storage
+let clienteSeteado = localStorage.getItem("cliente");
+if (cliente =null){
+    clienteSeteado = "BEJERMAN"
+}
+document.getElementById("razonsocial").value = clienteSeteado;
+
+let inputRazonSocial = document.getElementById("razonsocial");
+let botonRecordar = document.getElementById("btnRecordarCliente");
+
+localStorage.setItem("cliente",clienteSeteado);
+
+botonRecordar.onclick=()=>{
+    if(inputRazonSocial.value !==""){
+        clienteSeteado = inputRazonSocial.value
+    }
+    localStorage.setItem("cliente",clienteSeteado);
+}
+
+
+
+
+
+
+
+
+
+// if(modo==null){
+//     modo="light"
+// }
+// let principal=document.getElementById("principal");
+// let boton=document.getElementById("btnRecordarCliente");
+// document.body.className=modo;
+// principal.className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center "+modo;
+
+// localStorage.setItem("clienteFijo",modo);
+
+// boton.onclick=()=>{
+//     if(modo=="light"){
+//         document.body.className="dark";
+//         principal.className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center dark";
+//         modo="dark";
+//         boton.innerText="Light Mode";
+//     }else{
+//         document.body.className="light";
+//         principal.className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center light";
+//         modo="light";
+//         boton.innerText="Dark Mode";
+//     }
+//     localStorage.setItem("modo",modo);
+// }
+
+
